e636fa1cac2d8d8621f09e95b212a56f1485b18b
marie-rgb <akinyimarie3@gmail.com>
2024-01-24 01:28:47 -0800

    my commit

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    matrix[
+            [5,6],
+            [7,8]
+            ]
+    new_matrix = square_matrix_simple(matrix)
+    print(new_matrix)
+    print(matrix)
+
+    
